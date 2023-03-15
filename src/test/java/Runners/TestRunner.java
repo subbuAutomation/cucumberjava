f3cd04affdcb5ca73832a2408cc29a47c23a79b5
RUPAMDEVELOPER1 <rupamsethi66@gmail.com>
2023-03-15 09:14:40 +0530

    update in feature file

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\selenumintellija\\jayporecuccumber\\src\\test\\Features",
+@CucumberOptions(features = "src/test/Features",
         glue="stepdefinitons",stepNotifications =true)
 
 public class TestRunner {
--- src/test/java/Runners/Testngruunner.java
+++ src/test/java/Runners/Testngruunner.java
@@ -4,7 +4,7 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "C:\\selenumintellija\\jayporecuccumber\\src\\test\\Features",
+@CucumberOptions(features = "src/test/Features",
         glue="stepdefinitons",plugin = {"pretty","html:target/cucumberreport.html"})
 public class Testngruunner extends AbstractTestNGCucumberTests {
 
